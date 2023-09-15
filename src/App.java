f4b7e510203c51c7cc06af35ce13dc12fad6bffb
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-09-15 12:19:00 -0300

    .

--- src/App.java
+++ src/App.java
@@ -6,22 +6,77 @@ public class App {
         // Mensagem de boas-vindas
         JOptionPane.showMessageDialog(null, "Seja bem-vindo(a) ao sistema da Loja SPCar");
 
+        int opcao, submenu;
+
+        opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha uma opção" + 
+        "\n1 - Sou usuário" +
+        "\n2 - Sou administrador"));
+
+        switch(opcao){
+            case 1:
+                JOptionPane.showMessageDialog(null,"Faça o login");
+                Autentificacao autentificacao = new Autentificacao();
+                if (!autentificacao.verificarLogin()) {
+                    return; // Encerra o programa se as credenciais forem incorretas
+                }
+
+                // Criação de uma instância da classe Fila
+                Fila fila = new Fila();
+                fila.verificarPrioridade();
+
+                // Criação de uma instância da classe AluguelCarro
+                AluguelCarro aluguelCarro = new AluguelCarro();
+                aluguelCarro.calcularAluguel();
+                break;
+
+            case 2:
+                AutentificacaoAdmin autentificacaoAdmin = new AutentificacaoAdmin();
+                if (!autentificacaoAdmin.verificarLogin()) {
+                    return; // Encerra o programa se as credenciais forem incorretas
+                }
+
+                // Criar uma instância da classe AdminMenu
+                AdminMenu adminMenu = new AdminMenu();
+                adminMenu.menuOpcao();
+                break;
+
+            default:
+            JOptionPane.showMessageDialog(null, "Opção invalida");
+        }
+
+    }
+    
+}
+
+class AutentificacaoAdmin {
+
+    public boolean verificarLogin (){
         String login = JOptionPane.showInputDialog("Digite o seu login");
         String senha = JOptionPane.showInputDialog("Digite a sua senha");
 
-        if (login.equals("aluno") && senha.equals("aluno")) {
+        if (login.equals("admin") && senha.equals("admin")) {
             JOptionPane.showMessageDialog(null, "Acesso Liberado");
+            return true; // Credenciais corretas
         } else {
             JOptionPane.showMessageDialog(null, "Login ou senha incorretos");
+            return false; // Credenciais incorretas
         }
+    }
+}
 
-        // Criação de uma instância da classe Fila
-        Fila fila = new Fila();
-        fila.verificarPrioridade();
-        
-        // Criação de uma instância da classe AluguelCarro
-        AluguelCarro aluguelCarro = new AluguelCarro();
-        aluguelCarro.calcularAluguel();
+class Autentificacao {
+
+    public boolean verificarLogin (){
+        String login = JOptionPane.showInputDialog("Digite o seu login");
+        String senha = JOptionPane.showInputDialog("Digite a sua senha");
+
+        if (login.equals("cliente") && senha.equals("cliente")) {
+            JOptionPane.showMessageDialog(null, "Acesso Liberado");
+            return true; // Credenciais corretas
+        } else {
+            JOptionPane.showMessageDialog(null, "Login ou senha incorretos");
+            return false; // Credenciais incorretas
+        }
     }
 }
 
@@ -63,6 +118,47 @@ class AluguelCarro {
 
         total = (dias * diarias) + (km * taxaKm);
 
-        JOptionPane.showMessageDialog(null, "O valor a ser pago pelo aluguel do " + modelo + " é " + total);
+        JOptionPane.showMessageDialog(null, "Nos cobramos um valor fixo de diaria de " + diarias);
+        JOptionPane.showMessageDialog(null, "Então o valor a ser pago pelo aluguel do " + modelo + " é " + total);
+    }
+}
+
+class AdminMenu {
+
+    public void menuOpcao() {
+        int submenu;
+
+        submenu = Integer.parseInt(JOptionPane.showInputDialog(null, "Menu de alteração de dados" + 
+                "\n1 - Alterar nome" +
+                "\n2 - Alterar endereço" +
+                "\n3 - Alterar telefone" + 
+                "\n4 - Alterar RG" + 
+                "\n5 - Excluir usuário"));
+
+        switch(submenu){
+            case 1:
+                JOptionPane.showMessageDialog(null,"Alterar nome selecionado");
+                break;
+
+            case 2:
+                JOptionPane.showMessageDialog(null,"Alterar endereço selecionado");
+                break;
+
+            case 3:
+                JOptionPane.showMessageDialog(null,"Alterar telefone selecionado");
+                break;
+
+            case 4:
+                JOptionPane.showMessageDialog(null,"Alterar RG selecionado");
+                break;
+
+            case 5:
+                JOptionPane.showMessageDialog(null,"Excluir usuário selecionado");
+                break;
+
+            default:
+                JOptionPane.showMessageDialog(null,"Opção invalida");
+                break;
+        }
     }
 }
